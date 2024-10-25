144eb57e4e2cf21bb0d5750f31388b7f31fcb1fc
Yunus_234 <147723693+SaydakhmedovYunus@users.noreply.github.com>
2024-10-25 17:13:18 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ function closePopup() {
     let close = document.getElementsByClassName('popup')[0];
     close.style.display = "none";
 }
+
+
+let searchButton = document.getElementById('searcher_1');
+searchButton.addEventListener('click', openSearcher);
+function openSearcher () {
+    console.log('Поисковик открылся');
+    let searcherFirst = document.getElementById('searcherFirst');
+    searcherFirst.style.display = 'block';
+}
+
+let closeSearcher = document.getElementById('closedEffect');
+closeSearcher.addEventListener('click', closeSearcherEnd);
+
+function closeSearcherEnd() {
+    let searcherFirst = document.getElementById('searcherFirst');
+    searcherFirst.style.display = 'none';
+}
+
